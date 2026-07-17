50b762aa09df65d28a6591f06e414944eee31bbb
SrCharlied <145832124+SrCharlied@users.noreply.github.com>
2026-07-17 17:53:46 -0600

    Poligonos y lineas

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,52 @@ fn main() {
     framebuffer.clear();
 
     framebuffer.set_current_color(0xFF0000);
-    framebuffer.line(100, 100, 600, 500);
-    framebuffer.line(100, 100, 600, 400);
-    framebuffer.line(100, 100, 600, 300);
-    framebuffer.line(100, 100, 600, 200);
+    //Polígono 1: (165, 380) (185, 360) (180, 330) (207, 345) (233, 330) (230, 360) (250, 380) (220, 385) (205, 410) (193, 383)
+    framebuffer.line(165, 380, 185, 360);
+    framebuffer.line(185, 360, 180, 330);
+    framebuffer.line(180, 330, 207, 345);
+    framebuffer.line(207, 345, 233, 330);
+    framebuffer.line(233, 330, 230, 360);
+    framebuffer.line(230, 360, 250, 380);
+    framebuffer.line(250, 380, 220, 385);
+    framebuffer.line(220, 385, 205, 410);
+    framebuffer.line(205, 410, 193, 383);
+    framebuffer.line(193, 383, 165, 380);
+    //Polígono 2: (321, 335) (288, 286) (339, 251) (374, 302)
+    framebuffer.line(321, 335, 288, 286);
+    framebuffer.line(288, 286, 339, 251);
+    framebuffer.line(339, 251, 374, 302);
+    framebuffer.line(374, 302, 321, 335);
+    //polígono 3: (377, 249) (411, 197) (436, 249)
+    framebuffer.line(377, 249, 411, 197);
+    framebuffer.line(411, 197, 436, 249);
+    framebuffer.line(436, 249, 377, 249);
+    //polígono 4: (413, 177) (448, 159) (502, 88) (553, 53) (535, 36) (676, 37) (660, 52)(750, 145) (761, 179) (672, 192) (659, 214) (615, 214) (632, 230) (580, 230)(597, 215) (552, 214) (517, 144) (466, 180)
+    framebuffer.line(413, 177, 448, 159);
+    framebuffer.line(448, 159, 502, 88);
+    framebuffer.line(502, 88, 553, 53);
+    framebuffer.line(553, 53, 535, 36);
+    framebuffer.line(535, 36, 676, 37);
+    framebuffer.line(676, 37, 660, 52);
+    framebuffer.line(660, 52, 750, 145);
+    framebuffer.line(750, 145, 761, 179);
+    framebuffer.line(761, 179, 672, 192);
+    framebuffer.line(672, 192, 659, 214);
+    framebuffer.line(659, 214, 615, 214);
+    framebuffer.line(615, 214, 632, 230);
+    framebuffer.line(632, 230, 580, 230);
+    framebuffer.line(580, 230, 597, 215);
+    framebuffer.line(597, 215, 552, 214);
+    framebuffer.line(552, 214, 517, 144);
+    framebuffer.line(517, 144, 466, 180);
+    framebuffer.line(466, 180, 413, 177);
+    //polígono 5: (682, 175) (708, 120) (735, 148) (739, 170)
+    framebuffer.line(682, 175, 708, 120);
+    framebuffer.line(708, 120, 735, 148);
+    framebuffer.line(735, 148, 739, 170);
+    framebuffer.line(739, 170, 682, 175);
+    //línea
+    framebuffer.line(100, 50, 200, 100);
     framebuffer.point(0,0);
 
     let _ = framebuffer.render_buffer("output.png");
