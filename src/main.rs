656b4a2cc479cb2a874ececa4983339cfd69a97d
SrCharlied <145832124+SrCharlied@users.noreply.github.com>
2026-07-17 20:21:51 -0600

    Fix final

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     framebuffer.set_background_color(0x000000);
     framebuffer.clear();
 
-    framebuffer.set_current_color(0xFFFFFF);
+    framebuffer.set_current_color(0xFFFF00);
     //Polígono 1: (165, 380) (185, 360) (180, 330) (207, 345) (233, 330) (230, 360) (250, 380) (220, 385) (205, 410) (193, 383)
     framebuffer.line(165, 380, 185, 360);
     framebuffer.line(185, 360, 180, 330);
@@ -27,15 +27,18 @@ fn main() {
     framebuffer.line(205, 410, 193, 383);
     framebuffer.line(193, 383, 165, 380);
     //Polígono 2: (321, 335) (288, 286) (339, 251) (374, 302)
+    framebuffer.set_current_color(0xFF0000);
     framebuffer.line(321, 335, 288, 286);
     framebuffer.line(288, 286, 339, 251);
     framebuffer.line(339, 251, 374, 302);
     framebuffer.line(374, 302, 321, 335);
     //polígono 3: (377, 249) (411, 197) (436, 249)
+    framebuffer.set_current_color(0x00FF00);
     framebuffer.line(377, 249, 411, 197);
     framebuffer.line(411, 197, 436, 249);
     framebuffer.line(436, 249, 377, 249);
     //polígono 4: (413, 177) (448, 159) (502, 88) (553, 53) (535, 36) (676, 37) (660, 52)(750, 145) (761, 179) (672, 192) (659, 214) (615, 214) (632, 230) (580, 230)(597, 215) (552, 214) (517, 144) (466, 180)
+    framebuffer.set_current_color(0x0000FF);
     framebuffer.line(413, 177, 448, 159);
     framebuffer.line(448, 159, 502, 88);
     framebuffer.line(502, 88, 553, 53);
@@ -60,20 +63,22 @@ fn main() {
     framebuffer.line(735, 148, 739, 170);
     framebuffer.line(739, 170, 682, 175);
     //línea
+    framebuffer.set_current_color(0xFFFFFF);
     framebuffer.line(100, 50, 200, 100);
     // pintar
     let poligono1 = [(165, 380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)];
     let poligono2 = [(321, 335), (288, 286), (339, 251), (374, 302)];
     let poligono3 = [(377, 249), (411, 197), (436, 249)];
-    let poligono4 = [(413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37), (660, 52), (750, 145), (761, 179), (672, 192), (659, 214), (615, 214), (632, 230), (580, 230), (597, 215), (552, 214), (517, 144), (466, 180), (682, 175), (708, 120), (735, 148), (739, 170)];
+    let poligono4 = [(413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37), (660, 52), (750, 145), (761, 179), (672, 192), (659, 214), (615, 214), (632, 230), (580, 230), (597, 215), (552, 214), (517, 144), (466, 180)];
+    let poligono5 = [(682, 175), (708, 120), (735, 148), (739, 170)];
     framebuffer.set_current_color(0xFFFF00);
-    framebuffer.scan(&poligono1);
+    framebuffer.scan(&[&poligono1]);
     framebuffer.set_current_color(0xFF0000);
-    framebuffer.scan(&poligono2);
+    framebuffer.scan(&[&poligono2]);
     framebuffer.set_current_color(0x00FF00);
-    framebuffer.scan(&poligono3);
+    framebuffer.scan(&[&poligono3]);
     framebuffer.set_current_color(0x0000FF);
-    framebuffer.scan(&poligono4);
+    framebuffer.scan(&[&poligono4, &poligono5]);
 
     let _ = framebuffer.render_buffer("output.png");
 
--- src/scanline.rs
+++ src/scanline.rs
@@ -1,31 +1,35 @@
 use crate::framebuffer::Framebuffer;
 
 pub trait Scan {
-    fn scan(&mut self, vertices: &[(i32, i32)]);
+    fn scan(&mut self, polygons: &[&[(i32, i32)]]);
 }
 
 impl Scan for Framebuffer {
-    fn scan(&mut self, vertices: &[(i32, i32)]) {
+    fn scan(&mut self, polygons: &[&[(i32, i32)]]) {
 
-        let mut y_min = vertices[0].1;
-        let mut y_max = vertices[0].1;
+        let mut y_min = polygons[0][0].1;
+        let mut y_max = polygons[0][0].1;
 
-        for v in vertices {
-            if v.1 < y_min {
-                y_min = v.1;
-            }
-            if v.1 > y_max {
-                y_max = v.1;
+        for poly in polygons {
+            for v in *poly {
+                if v.1 < y_min {
+                    y_min = v.1;
+                }
+                if v.1 > y_max {
+                    y_max = v.1;
+                }
             }
         }
 
         for y in y_min..=y_max {
             let mut cruces: Vec<i32> = Vec::new();
-            for i in 0..vertices.len(){
-                let a = vertices[i];
-                let b = vertices[(i + 1) % vertices.len()];
-                if (a.1 <= y && b.1 > y) || (a.1 > y && b.1 <= y) {
-                    cruces.push((b.0 - a.0) * (y - a.1) / (b.1 - a.1) + a.0);
+            for poly in polygons {
+                for i in 0..poly.len(){
+                    let a = poly[i];
+                    let b = poly[(i + 1) % poly.len()];
+                    if (a.1 <= y && b.1 > y) || (a.1 > y && b.1 <= y) {
+                        cruces.push((b.0 - a.0) * (y - a.1) / (b.1 - a.1) + a.0);
+                    }
                 }
             }
             cruces.sort();
@@ -37,6 +41,6 @@ impl Scan for Framebuffer {
                 }
             }
         }
-    
+
     }
 }
